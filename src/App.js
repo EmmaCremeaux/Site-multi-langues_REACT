6293c50eb3443baf2edfaee70597dfbd74c61a69
EmmaCremeaux <122892031+EmmaCremeaux@users.noreply.github.com>
2023-01-26 17:01:25 +0100

    -

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import contenu from './components/dataBase';
+import { changeLanguage, useState } from 'react';
+
 
 function App() {
+
+  const [languageDefault, setLanguageDefault] = useState("en-EN")
+
+  const changeLanguage = (event) => {
+    setLanguageDefault(event.target.alt)
+    console.log(languageDefault)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className = "contain">
+  <nav> 
+    <ul>
+      {contenu.map((unContenu, i) => (
+      <li className="imagePays"><img onClick={changeLanguage} src={`assets/${unContenu.languages}.svg`} alt={unContenu.languages} /></li>
+      ))}
+    </ul>
+  </nav>
+ <div className="form">
+  <h1 className="titleForm">
+      {contenu[0].title}
+  </h1>
+
+    <h2 className="subtitleForm">
+      {contenu[0].subtitle}
+    </h2>
     </div>
-  );
+</div>
+ )
 }
+
+
+
+
+// OK 1- Se créer notre fichier de données. 
+// 2- Se créer un composant contenu pour tester l'affichage de notre jeu de données.
+// 3- Se créer un contexte en fonction de la langue.
+// 4- Se créer un composant avec nos icônes pour changer la langue en fonction du contexte.
+// 5- Récupérer la langue du navigateur et log le résultat.
+// 6- Faire appel à la langue récupérée si elle existe sinon utiliser une langue par défaut (Anglais).
+
 
 export default App;
---
+++ src/components/dataBase.js
@@ -0,0 +1,10 @@
+
+
+
+const contenu =[
+    { languages: "en-EN", title:"Hello and welcome to the site.", subtitle:"Discover our features and services!" },
+    { languages: "fr-FR", title:"Bonjour et bienvenue sur le site.", subtitle:"Découvrez nos fonctionnalités et nos services!" },
+    { languages: "es-ES", title:"hola y bienvenido al sitio.", subtitle:"Descubre nuestras características y servicios!" },
+];
+
+export default contenu; 
